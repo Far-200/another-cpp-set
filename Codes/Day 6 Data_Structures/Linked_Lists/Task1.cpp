c422a98edcb2c77a0a578b573b567bc9a276acf4
Far-200 <farhaabkhanff@gmail.com>
2026-09-14 00:05:04 +0530

    Linked Lists: Learned how to create a LL and insert at front and end💔

--- Codes/Day 6 Data_Structures/Linked_Lists/Task1.cpp
+++ Codes/Day 6 Data_Structures/Linked_Lists/Task1.cpp
@@ -27,12 +27,45 @@ int main(){
     
     //Well.........shit
     Node* temp = head;
-    while(temp != nullptr){
-        cout << temp->data << " ";
-        count++;
+
+    while (temp->next != nullptr) {
         temp = temp->next;
     }
 
-    cout << "\nNumber of Nodes: " << count << endl;
+    //cout << "\nNumber of Nodes: " << count << endl;
+
+    //Insertion
+    Node* newNode = new Node();
+    newNode->data = 10;
+    newNode->next = head;
+    head = newNode;
+    //Node* temp = head;
+    /*while(temp != nullptr){
+        cout << temp->data << " ";
+        count++;
+        temp = temp->next;
+    }*/
+    //head->next = nullptr;
+    //cout << "\nLet's see...: " << head->data << endl; 
+    //10 should come....but how?
+    //Could use newNode->data, but that defeats the purpose of linking from head.
+
+    //insertion at end
+    Node* tail = new Node();
+    
+    tail->data = 100;
+    tail->next = nullptr;
+    temp->next = tail;
+
+    cout << "Please work🫠: " << tail->data << endl;
+
+    //Now to print full list...
+    //How but?
+    Node* curr = new Node();
+    curr = head;
+    while(curr != nullptr){
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
     return 0;
 }
